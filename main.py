873f9f3c821e959e650b40e88e4e3b4434711bf0
Vladislav <exs2199@gmail.com>
2021-01-16 22:04:47 +0200

    init main object

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-from win10toast import ToastNotifier
+from win32api import GetSystemDirectory, GetUserName
+import os
 
-toaster = ToastNotifier()
-toaster.show_toast('Sample', 'Python')
+drive = GetSystemDirectory().split(':')[0]
+user = GetUserName()
+download_folders = [f'{drive}:\\Users\\{user}\\Downloads',
+                    f'{drive}:\\Пользователи\\{user}\\Загрузки']
+
+
+class Main:
+    def __init__(self, folders: list):
+        self.folders = folders
+        self.folder_path = None
+
+    def check_path(self):
+        for folder in self.folders:
+            if os.path.isdir(folder):
+                self.folder_path = folder
+                break
+
+
+if __name__ == '__main__':
+    inst = Main(download_folders)
+    inst.check_path()
